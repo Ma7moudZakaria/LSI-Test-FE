56a77bc2ce09b3f451e64ccd003bbf4c8ed29b0e
msalah <msalah@arabdt.com>
2021-02-23 13:26:05 +0200

    Attachments Service

--- src/app/core/interfaces/attachments-interfaces/iattachment.ts
+++ src/app/core/interfaces/attachments-interfaces/iattachment.ts
@@ -1,2 +1,5 @@
 export interface IAttachment {
+    containerNameIndex:number,
+    file:File,
+
 }
--- src/app/core/services/attachments-services/attachments.service.ts
+++ src/app/core/services/attachments-services/attachments.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { IAttachment } from '../../interfaces/attachments-interfaces/iattachment';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +9,26 @@ import { HttpClient } from '@angular/common/http';
 export class AttachmentsService {
 
   constructor(private http: HttpClient) { }
+  UploadFilesURL =environment.BaseURL+"Attachments/upload-file";
+  getAttachmentId=environment.BaseURL+"Attachments/download-file/";
+  deleteAttachmentId=environment.BaseURL+"Attachments/delete-file/";
+  
+  upload(files:IAttachment[]) {
+
+    const fd = new FormData();
+    Array.from(files).forEach((file) => {
+      fd.append('attachmentsModel'+[0]+'ContainerNameIndex', file.containerNameIndex.toString());
+      fd.append('attachmentsModel'+[0]+'File', file.file);
+
+    });
+    return this.http.post(this.UploadFilesURL,fd)
+  }
+   
+  getFile(filesIds:Array<string>){
+    return this.http.post(this.getAttachmentId, filesIds);
+  }
+  
+  removeFile(fileId:string){
+    return this.http.delete(this.deleteAttachmentId+fileId);
+  }
 }
