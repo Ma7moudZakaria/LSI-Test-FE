294e6e522aa460d8b346ae279338cddac921af38
alaa <ahaggag@arabdt.com>
2021-03-05 18:41:17 +0200

    update Name Confidention

--- src/app/modules/question-bank/components/quesion-bank-view/question-bank-categories-view/question-bank-categories-view.component.ts
+++ src/app/modules/question-bank/components/quesion-bank-view/question-bank-categories-view/question-bank-categories-view.component.ts
@@ -16,48 +16,35 @@ import { QuestionBankCategoryService } from 'src/app/core/services/question-bank
   styleUrls: ['./question-bank-categories-view.component.scss']
 })
 export class QuestionBankCategoriesViewComponent implements OnInit {
-  //currentWindowWidth?: number;
-  //smallScreen: number = 426;
-  //valueLang = "nameAr";
   filterErrorMessage?:string;
-  QuestionBankCategoryList: IquestionBankCategoriesModel[] = []; ;
-  QuestionBankCategoryFilter: IquestionBankCategoriesFilter = {};
+  questionBankCategoryList: IquestionBankCategoriesModel[] = []; ;
+  questionBankCategoryFilter: IquestionBankCategoriesFilter = {};
   position: string="";
-  //msgs: Message[] = [];
   isView: boolean = true;
-  Title?: string;
-  QuestionBankCategoryId:string='';
-  QuestionBankCategory?: IquestionBankCategoriesModel ;
-  QuestionBankCategoryCreat: IquestionBankCategoryCreatModel = {};
-  QuestionBankCategoryUpdate: IquestionBankCategoryUpdateModel = {} ;
+  title?: string;
+  questionBankCategoryId:string='';
+  questionBankCategory?: IquestionBankCategoriesModel ;
+  questionBankCategoryCreat: IquestionBankCategoryCreatModel = {};
+  questionBankCategoryUpdate: IquestionBankCategoryUpdateModel = {} ;
   isAdd:boolean=true;
-  // currentWindowWidth?: number;
   errorMessage?:string;
   maxDate: any;
-  // msgs: Message[] = [];
    currentForm: FormGroup=new FormGroup({});
    formImport: FormGroup;
   successMessage?:string;
   isSubmit = false;
-
 @Output() selectedCategoryId= new EventEmitter<string>();
 clickChangeCtogry:string="";
   constructor(private questionBankCategoryService: QuestionBankCategoryService,
     private activeroute: ActivatedRoute, 
     private router: Router, public translate: TranslateService,private fb: FormBuilder) { 
-
       this.formImport = new FormGroup({
         importFile: new FormControl('', Validators.required)
       });
     }
-
   ngOnInit(): void {
-    // this.currentWindowWidth = window.innerWidth;
-    // this.valueLang= this.translate.currentLang=='en-US'?'nameEn':'nameAr';
-    // this.nav.show();
     this.getQuestionBankCategories()
     this.buildForm();
-
   }
   get f() {
     return this.currentForm?.controls;
@@ -65,30 +52,25 @@ clickChangeCtogry:string="";
   buildForm() {
     this.currentForm = this.fb.group(
       {
-       
         nameAr: ['', Validators.required],
         nameEn: ['', Validators.required],
-      
-
-
       })
   }
   getQuestionBankCategories(name?:string) {
     this.isView=true;
-    this.QuestionBankCategoryId="";
+    this.questionBankCategoryId="";
     this.filterErrorMessage = "";
-    if(name!=null||name!=""){ this.QuestionBankCategoryFilter.catgName=name;}
-   
-    this.QuestionBankCategoryFilter.pageNumber=1;
-    this.QuestionBankCategoryFilter.pageSize=10;
-    this.questionBankCategoryService.getQuestionBankCategoriesFilter(this.QuestionBankCategoryFilter).subscribe(res => {
+    if(name!=null||name!=""){ this.questionBankCategoryFilter.catgName=name;}
+    this.questionBankCategoryFilter.pageNumber=1;
+    this.questionBankCategoryFilter.pageSize=10;
+    this.questionBankCategoryService.getQuestionBankCategoriesFilter(this.questionBankCategoryFilter).subscribe(res => {
       let response = <BaseResponseModel>res;
       if (response.isSuccess) {
-        this.QuestionBankCategoryList = response.data;
-        this.loadCatogryQuiestion(this.QuestionBankCategoryList[0].id)
+        this.questionBankCategoryList = response.data;
+        this.loadCatogryQuiestion(this.questionBankCategoryList[0].id)
       }
       else {
-        this.QuestionBankCategoryList = [];
+        this.questionBankCategoryList = [];
         this.filterErrorMessage = response.message;
       }
     },
@@ -98,9 +80,9 @@ clickChangeCtogry:string="";
     )
   }
   clearFilter(){
-    this.QuestionBankCategoryFilter = {};
-    this.QuestionBankCategoryFilter.pageSize =0 ;
-    this.QuestionBankCategoryFilter.pageNumber = 10;
+    this.questionBankCategoryFilter = {};
+    this.questionBankCategoryFilter.pageSize =0 ;
+    this.questionBankCategoryFilter.pageNumber = 10;
     this.getQuestionBankCategories();
   }
 
@@ -116,40 +98,20 @@ alert("تم الحذف")
     )
   
   }
-
-  confirmPosition(position: string) {
-    this.position = position;
-
-    // this.confirmationService.confirm({
-    //   message: this.translate.currentLang == Languages.English ?'Do you want to delete this record?':'هل تريد حذف هذا السجل؟',
-    //   header:this.translate.currentLang == Languages.English ? 'Delete Confirmation':'تأكيد الحذف',
-    //   icon: 'pi pi-info-circle',
-    //   accept: () => {
-    //     // this.msgs = [{ severity: 'info', summary: 'Confirmed', detail: 'Record deleted' }];
-    //   },
-    //   reject: () => {
-    //     // this.msgs = [{ severity: 'info', summary: 'Rejected', detail: 'You have rejected' }];
-    //   },
-    //   key: "positionDialog"
-    // });
-  }
   ChangCTg(categoryId:string) {
-  
     this.router.navigateByUrl('/questionBank/question-bank-questions-view/'+categoryId);
- 
-    
    }
    loadQuestionBankCategoryDetails(id?:string) {
      this.isView=false;
      this.isAdd=false;
-    this.QuestionBankCategoryId=id ||'';
-    this.questionBankCategoryService.getQuestionBankCategoryDetails(this.QuestionBankCategoryId).subscribe(
+    this.questionBankCategoryId=id ||'';
+    this.questionBankCategoryService.getQuestionBankCategoryDetails(this.questionBankCategoryId).subscribe(
       res => {
         var response =<BaseResponseModel>res;
         if (response.isSuccess) {
-          this.QuestionBankCategory = response.data;
-          this.f.nameAr.setValue(this.QuestionBankCategory?.arabCatgName);
-      this.f.nameEn.setValue(this.QuestionBankCategory?.engCatgName);
+          this.questionBankCategory = response.data;
+          this.f.nameAr.setValue(this.questionBankCategory?.arabCatgName);
+      this.f.nameEn.setValue(this.questionBankCategory?.engCatgName);
         }
         else {
           this.errorMessage = response.message;
@@ -159,21 +121,17 @@ alert("تم الحذف")
       })
       
   }
-  PopulateForm() {
-  }
   Submit() {
-   
     this.isSubmit = true;
     this.errorMessage = '';
     this.successMessage = '';
-
-    if (this.QuestionBankCategoryId) {
-      this.QuestionBankCategoryUpdate.id=this.QuestionBankCategoryId;
-      this.QuestionBankCategoryUpdate.no=this.QuestionBankCategory?.no;
-      this.QuestionBankCategoryUpdate.arabCatgName=this.f.nameAr.value;
-      this.QuestionBankCategoryUpdate.engCatgName=this.f.nameEn.value;
+    if (this.questionBankCategoryId) {
+      this.questionBankCategoryUpdate.id=this.questionBankCategoryId;
+      this.questionBankCategoryUpdate.no=this.questionBankCategory?.no;
+      this.questionBankCategoryUpdate.arabCatgName=this.f.nameAr.value;
+      this.questionBankCategoryUpdate.engCatgName=this.f.nameEn.value;
     
-      this.questionBankCategoryService.UpdateQuestionBankCategory(this.QuestionBankCategoryUpdate).subscribe(res => {
+      this.questionBankCategoryService.UpdateQuestionBankCategory(this.questionBankCategoryUpdate).subscribe(res => {
         if (res.isSuccess) {
           this.isSubmit = false;
           this.successMessage = res.message;
@@ -195,9 +153,9 @@ alert("تم الحذف")
         })
     }
     else {
-      this.QuestionBankCategoryCreat.arabCatgName=this.f.nameAr.value;
-      this.QuestionBankCategoryCreat.engCatgName=this.f.nameEn.value;
-      this.questionBankCategoryService.addQuestionBankCategory(this.QuestionBankCategoryCreat).subscribe(res => {
+      this.questionBankCategoryCreat.arabCatgName=this.f.nameAr.value;
+      this.questionBankCategoryCreat.engCatgName=this.f.nameEn.value;
+      this.questionBankCategoryService.addQuestionBankCategory(this.questionBankCategoryCreat).subscribe(res => {
         this.isSubmit = false;
         if (res.isSuccess) {
           this.isView=true;
@@ -214,20 +172,15 @@ alert("تم الحذف")
           
         })
     }
-   
- 
-
   }
   addCatogry(){
     this.currentForm.reset();
 this.isView=false;
 this.isAdd=true;
-
   }
   back_list_Catogry(){
 this.isView=true;
 this.isAdd=false;
-
   }
 
  
--- src/app/modules/question-bank/components/quesion-bank-view/question-bank-questions-view/question-bank-questions-view.component.ts
+++ src/app/modules/question-bank/components/quesion-bank-view/question-bank-questions-view/question-bank-questions-view.component.ts
@@ -6,21 +6,15 @@ import { IquestionBankQuestionsFilterRequest } from 'src/app/core/interfaces/que
 import { IquestionBankQuestionsModel } from 'src/app/core/interfaces/questionBankQuestions-interfaces/iquestion-bank-questions-model';
 import { BaseResponseModel } from 'src/app/core/ng-model/base-response-model';
 import { QuestionBankQuestionService } from 'src/app/core/services/question-bank-services/question-bank-question.service';
-
 @Component({
   selector: 'app-question-bank-questions-view',
   templateUrl: './question-bank-questions-view.component.html',
   styleUrls: ['./question-bank-questions-view.component.scss']
 })
 export class QuestionBankQuestionsViewComponent implements OnInit {
-  //currentWindowWidth?: number;
-  //smallScreen: number = 426;
-  //valueLang = "nameAr";
   filterErrorMessage?:string;
-  QuestionBankQuestionList: IquestionBankQuestionsModel[] = [];
-  QuestionBankQuestionFilter: IquestionBankQuestionsFilterRequest = {};
-  // position: string="";
-  //msgs: Message[] = [];
+  questionBankQuestionList: IquestionBankQuestionsModel[] = [];
+  questionBankQuestionFilter: IquestionBankQuestionsFilterRequest = {};
   @Input() selectedCategoryId?:string; 
   @Output() selectedQuestionId = new EventEmitter<{}>();;
   @Output() isViewAdd = new EventEmitter<boolean>();;
@@ -28,33 +22,29 @@ export class QuestionBankQuestionsViewComponent implements OnInit {
      public translate: TranslateService) { }
 
   ngOnInit(): void {
-    // this.currentWindowWidth = window.innerWidth;
-    // this.valueLang= this.translate.currentLang=='en-US'?'nameEn':'nameAr';
-    // this.nav.show();
     this.getQuestionBankQuestions()
   }
   ngOnChanges(changes: any) {
-    // console.log(changes);
     this.getQuestionBankQuestions(changes.selectedCategoryId.currentValue);
     this.selectedCategoryId=changes.selectedCategoryId.currentValue;
   }
   searchQuestions(Questions?:string){
-    this.QuestionBankQuestionList=[];
+    this.questionBankQuestionList=[];
     this.getQuestionBankQuestions(this.selectedCategoryId,Questions) 
   }
   getQuestionBankQuestions(CategoryId?:any,Questions?:string) {
     this.filterErrorMessage = "";
-    this.QuestionBankQuestionFilter.PageNumber=1;
-    this.QuestionBankQuestionFilter.PageSize=10;
-    this.QuestionBankQuestionFilter.catgyId=CategoryId;
-    this.QuestionBankQuestionFilter.question=Questions;
-    this.questionBankQuestionService.getQuestionBankQuestionsFilter(this.QuestionBankQuestionFilter).subscribe(res => {
+    this.questionBankQuestionFilter.PageNumber=1;
+    this.questionBankQuestionFilter.PageSize=10;
+    this.questionBankQuestionFilter.catgyId=CategoryId;
+    this.questionBankQuestionFilter.question=Questions;
+    this.questionBankQuestionService.getQuestionBankQuestionsFilter(this.questionBankQuestionFilter).subscribe(res => {
       let response = <BaseResponseModel>res;
       if (response.isSuccess) {
-        this.QuestionBankQuestionList = response.data;
+        this.questionBankQuestionList = response.data;
       }
       else {
-        this.QuestionBankQuestionList = [];
+        this.questionBankQuestionList = [];
         this.filterErrorMessage = response.message;
       }
     },
@@ -63,11 +53,10 @@ export class QuestionBankQuestionsViewComponent implements OnInit {
       }
     )
   }
-  
   clearFilter(){
-    this.QuestionBankQuestionFilter = {};
-    this.QuestionBankQuestionFilter.PageNumber=10;
-    this.QuestionBankQuestionFilter.PageSize=0;
+    this.questionBankQuestionFilter = {};
+    this.questionBankQuestionFilter.PageNumber=10;
+    this.questionBankQuestionFilter.PageSize=0;
     this.getQuestionBankQuestions(this.selectedCategoryId);
   }
 
@@ -83,30 +72,10 @@ export class QuestionBankQuestionsViewComponent implements OnInit {
     )
   
   }
-
-  confirmPosition(position: string) {
-    // this.position = position;
-
-    // this.confirmationService.confirm({
-    //   message: this.translate.currentLang == Languages.English ?'Do you want to delete this record?':'هل تريد حذف هذا السجل؟',
-    //   header:this.translate.currentLang == Languages.English ? 'Delete Confirmation':'تأكيد الحذف',
-    //   icon: 'pi pi-info-circle',
-    //   accept: () => {
-    //     this.msgs = [{ severity: 'info', summary: 'Confirmed', detail: 'Record deleted' }];
-    //   },
-    //   reject: () => {
-    //     this.msgs = [{ severity: 'info', summary: 'Rejected', detail: 'You have rejected' }];
-    //   },
-    //   key: "positionDialog"
-    // });
-  }
   loadQuestion(id?:string){
     this.selectedQuestionId?.emit({id: id, show : true });
-    // this.isViewAdd.emit(true);
   }
   NewQuestion(){
     this.selectedQuestionId?.emit({id: '', show : true });
-    // this.QuestionId?.emit("");
-    // this.isViewAdd.emit(true);
   }
 }
