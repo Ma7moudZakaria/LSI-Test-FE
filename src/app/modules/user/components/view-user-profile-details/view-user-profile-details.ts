9424a7dc8ebde8b624d406f1c03f5ef3579b3fd7
nermenOsama123 <70470826+nermenOsama123@users.noreply.github.com>
2021-03-10 18:29:37 +0200

    view-user-profile-details

--- src/app/modules/user/components/view-user-profile-details/view-user-profile-details.ts
+++ src/app/modules/user/components/view-user-profile-details/view-user-profile-details.ts
@@ -19,51 +19,59 @@ import { UserService } from 'src/app/core/services/user-services/user.service';
 export class ViewUserProfileDetailsComponent implements OnInit {
 
   RouteParams: any;
-  userProfileDetails : any;
-  isSuccess:any;
-  successMessage:any;
-  allLookups:any;
-  listOfLookupProfile: string[] = ['GENDER','EDU_LEVEL','NATIONALITY','COUNTRY'];
-  currentUser:any;
-  errorMessage:any;
+  userProfileDetails: any;
+  isSuccess: any;
+  successMessage: any;
+  allLookups: any;
+  listOfLookupProfile: string[] = ['GENDER', 'EDU_LEVEL', 'NATIONALITY', 'COUNTRY'];
+  currentUser: any;
+  errorMessage: any;
   collectionOfLookup = {} as ILookupCollection;
-  profileForm: FormGroup  = new FormGroup({})
-  genderData :any;
-  countryData :any;
-  nationalityData :any;
-  educationalLevelData :any;
-  language:any;
+  profileForm: FormGroup = new FormGroup({})
+  genderData: any;
+  countryData: any;
+  nationalityData: any;
+  educationalLevelData: any;
+  language: any;
   langEnum = LanguageEnum;
-  birthdate:any;
+  birthdate: any;
 
   constructor(
     private router: Router,
     private fb: FormBuilder,
-    public translate : TranslateService,
-    private userService:UserService) { 
+    public translate: TranslateService,
+    private userService: UserService) {
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.currentUser = JSON.parse(localStorage.getItem("user") as string) as IUser;
     this.language = this.language === LanguageEnum.ar ? LanguageEnum.en : LanguageEnum.ar;
     this.userProfileDetails as IUserProfile;
     this.RouteParams = this.router.url;
-    this.getUserProfile(this.currentUser.id);    
+    this.getUserProfile(this.currentUser.id);
   }
 
-  getUserProfile(id : any)
-  {
-    this.userService.viewUserProfileDetails(id).subscribe(res =>{
-      
+  getUserProfile(id: any) {
+    this.userService.viewUserProfileDetails(id).subscribe(res => {
+
       this.userProfileDetails = res.data;
       this.birthdate = new Date(this.userProfileDetails.birthdate.toString());
       this.birthdate = new Date(this.birthdate.setDate(this.birthdate.getDate() + 1)).toISOString().slice(0, 10);
 
-      if (res.isSuccess){
+      if (res.isSuccess) {
       }
-      else{
-        this.errorMessage  = res.message;
+      else {
+        this.errorMessage = res.message;
       }
     });
+  }
+
+  //  openNav() {
+  //   document.getElementById("mySidenav").style.width = "250px";
+  // }
+
+  /* Set the width of the side navigation to 0 */
+  closeNav() {
+    // document.getElementById("mySidenav").style.width = "0";
   }
 }
