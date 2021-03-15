01f182f9897c7119249e6bad4eae745f0be28b7f
msalah <msalah@arabdt.com>
2021-03-15 10:49:38 +0200

    materila filter FE

--- src/app/modules/scientific-material/components/scientific-material-view/material-list/material-list.component.ts
+++ src/app/modules/scientific-material/components/scientific-material-view/material-list/material-list.component.ts
@@ -3,6 +3,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { LanguageEnum } from 'src/app/core/enums/language-enum.enum';
 import { IScientificMaterialFilter } from 'src/app/core/interfaces/scientific-material/iscientific-matrial-filter';
 import { IScientificMaterialGrid } from 'src/app/core/interfaces/scientific-material/iscientific-matrial-grid';
+import { BaseLookupModel } from 'src/app/core/ng-model/base-lookup-model';
 import { ScientificMaterialService } from 'src/app/core/services/scientific-material-services/scientific-material.service';
 
 @Component({
@@ -16,19 +17,22 @@ export class MaterialListComponent implements OnInit {
   @Input() selectedProgramId?: string;
   @Output() materialId = new EventEmitter<string>();
   langEnum = LanguageEnum;
+  materialCategoires : BaseLookupModel [] =[];
+  LangEnum = LanguageEnum ;
   constructor(private scientifcMaterialService: ScientificMaterialService, public translate: TranslateService) { }
 
   ngOnInit(): void {
+    this.loadMaterialCategories();
     this.loadProgramMaterial();
   }
   ngOnChanges(changes: any) {
     console.log(changes);
-    this.loadProgramMaterial(changes.selectedProgramId.currentValue);
+    this.materialFilter.programs = changes.selectedProgramId.currentValue;
+    this.loadProgramMaterial();
   }
-  loadProgramMaterial(programId?: any) {
+  loadProgramMaterial() {
     this.materialFilter.skip = 0;
-    this.materialFilter.take = 10;
-    this.materialFilter.programs = programId ? programId : undefined
+    this.materialFilter.take = 10;    
     this.scientifcMaterialService.getScientificMateriaFilter(this.materialFilter).subscribe(
       (res: any) => {
         this.materials = res.data as IScientificMaterialGrid[];
@@ -48,6 +52,21 @@ export class MaterialListComponent implements OnInit {
       }, error => {
         console.log(error);
       })
+  }
+  
+  loadMaterialCategories() {
+    
+    this.scientifcMaterialService.GetScientificMatrialCategoriesLookup().subscribe(
+      (res: any) => {
+        this.materialCategoires = res.data as any[];        
+      }, error => {
+        console.log(error);
+      }
+    );
+  }
+  changeCategory(categoryId?:string){
+    this.materialFilter.categoryId =categoryId;
+    this.loadProgramMaterial();
   }
 
 }
