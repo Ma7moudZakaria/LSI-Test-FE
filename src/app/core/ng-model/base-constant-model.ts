cb92de2d808a662c4ab6fa623bcd649a3674f9fa
alaa <ahaggag@arabdt.com>
2021-03-28 13:37:24 +0200

    done active code

--- src/app/core/ng-model/base-constant-model.ts
+++ src/app/core/ng-model/base-constant-model.ts
@@ -6,8 +6,11 @@ export class BaseConstantModel {
         '/auth/(baseRouter:reset-password)', 
         '/auth/(baseRouter:activate-code)']
 
-        public static SUCCESS_TYPE = 'success'
-        public static DANGER_TYPE = 'danger'
+        public static SUCCESS_TYPE = 'success';
+        public static DANGER_TYPE = 'danger';
+        public static  Success_Status_Code = 200;
+        public static  Failure_Status_Code = 0;
+        public static  Bad_Request_Status_Code = 400;
 
         public static dashboard = '../../../../assets/images/sidebar/Dashboard-icon.png';
 }
