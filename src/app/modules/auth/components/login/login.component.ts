5a085eca9450adb0fcc10ef848250829984c0e8b
alaa <ahaggag@arabdt.com>
2021-04-05 13:45:12 +0200

    done

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -61,7 +61,7 @@ export class LoginComponent implements OnInit {
     // });
 
     this.userform = this.fb.group({
-      email: ["", [Validators.required, Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      email: ["", [Validators.required]],//, Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
       password: ["", [Validators.required, Validators.minLength(6) , Validators.maxLength(12)]],
     });
     
