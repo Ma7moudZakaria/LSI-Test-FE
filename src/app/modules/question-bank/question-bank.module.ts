4bdd7e5fa585b0e120cf9f303ed7bc3e925eceb1
nermenOsama123 <70470826+nermenOsama123@users.noreply.github.com>
2021-04-08 19:02:21 +0200

    pushed notify

--- src/app/modules/question-bank/question-bank.module.ts
+++ src/app/modules/question-bank/question-bank.module.ts
@@ -12,16 +12,17 @@ import { AddQuestionBankQuestionComponent } from './components/quesion-bank-view
 import { QuestionBankQuestionsViewComponent } from './components/quesion-bank-view/question-bank-questions-view/question-bank-questions-view.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AddQuestionBankCategoryComponent } from './components/quesion-bank-view/add-question-bank-category/add-question-bank-category.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-  declarations: [AddQuestionBankCategoryComponent ,QuestionBankCategoriesViewComponent, QuestionBankCategoryDetailsComponent, AddQuestionBankQuestionComponent,
-     QuestionBankQuestionsViewComponent, QuestionBankQuestionDetailsComponent, QuesionBankViewComponent],
+  declarations: [AddQuestionBankCategoryComponent, QuestionBankCategoriesViewComponent, QuestionBankCategoryDetailsComponent, AddQuestionBankQuestionComponent,
+    QuestionBankQuestionsViewComponent, QuestionBankQuestionDetailsComponent, QuesionBankViewComponent],
   imports: [
     SharedModule,
     FormsModule,
     ReactiveFormsModule,
     CommonModule,
-    QuestionBankRoutingModule, TranslateModule
-  ]
+    QuestionBankRoutingModule, TranslateModule, MatTooltipModule
+  ], providers: [MatTooltipModule]
 })
 export class QuestionBankModule { }
