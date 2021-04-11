40e543727c2a2b7420b090665ca95d8c97ce4cff
nermenOsama123 <70470826+nermenOsama123@users.noreply.github.com>
2021-04-11 19:17:46 +0200

    overlay in all page

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -15,17 +15,34 @@ export class HeaderComponent implements OnInit {
 
   currentLang = '';
   showPro = true;
+  disableBlock = false;
   submitClose = false;
   title = '';
   lang = LanguageEnum;
-  constructor(private langService: LanguageService, public translate : TranslateService) { }
+  constructor(private langService: LanguageService, public translate: TranslateService) { }
 
   ngOnInit(): void {
     this.currentLang = JSON.parse(localStorage.getItem("lang") || '{}');
     this.titleSubscription();
   }
+  showoverlay() {
+    this.showPro = !this.showPro;
+    this.disableBlock = true;
 
-  titleSubscription(){
+  }
+  showoverlay_withoverlay() {
+    this.disableBlock = false;
+    this.showPro = true;
+    this.closeProfile(false);
+  }
+
+  closeProfile(event: boolean) {
+    this.submitClose = event;
+    this.showPro = true;
+
+  }
+
+  titleSubscription() {
     this.langService.headerPageNameEvent.subscribe(res => {
       this.title = res;
     })
@@ -45,9 +62,5 @@ export class HeaderComponent implements OnInit {
     // return this.currentLang === LanguageEnum.ar ? LanguageEnum.en.split('-')[0].toUpperCase() : LanguageEnum.ar.split('-')[0].toUpperCase();
     return this.currentLang === LanguageEnum.ar ? this.translate.instant('GENERAL.ENGLISH_DISPALY_SWITCH') : this.translate.instant('GENERAL.ARABIC_DISPALY_SWITCH')
   }
-  closeProfile(event: boolean) {
-    this.submitClose = event;
-    this.showPro = true
 
-  }
 }
