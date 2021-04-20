784f31f4fc5873276a9726f7c35bf1533eb9f0fa
alaa <ahaggag@arabdt.com>
2021-04-20 16:18:36 +0200

    done voice note

--- src/app/modules/shared-material/components/shared-material/shared-material.component.ts
+++ src/app/modules/shared-material/components/shared-material/shared-material.component.ts
@@ -5,6 +5,13 @@ import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogModel, ConfirmModalComponent } from 'src/app/shared/components/confirm-modal/confirm-modal.component';
 import { IDragDropAccordionItems } from 'src/app/core/interfaces/shared-interfaces/accordion-interfaces/idrag-drop-accordion-items';
 import { ITelInputParams } from 'src/app/core/interfaces/shared-interfaces/tel-input-interfaces/itel-input-params';
+import { DomSanitizer } from '@angular/platform-browser';
+import * as RecordRTC from 'recordrtc';
+import { AttachmentsService } from 'src/app/core/services/attachments-services/attachments.service';
+import { IAttachment } from 'src/app/core/interfaces/attachments-interfaces/iattachment';
+import { IFileUpload } from 'src/app/core/interfaces/attachments-interfaces/ifile-upload';
+import { BaseMessageModel } from 'src/app/core/ng-model/base-message-model';
+import { BaseConstantModel } from 'src/app/core/ng-model/base-constant-model';
 
 export interface DragDropListItem {
   id: string;
@@ -28,8 +35,20 @@ export class SharedMaterialComponent implements OnInit {
     isRequired : true,
     countryIsoCode: '{"initialCountry": "sa"}'
   }
-
-  constructor(public dialog: MatDialog) { }
+//===========recrd=================
+//Lets initiate Record OBJ
+ record:any;
+//Will use this flag for detect recording
+ recording = false;
+//Url of Blob
+ url:any;
+ error:any;
+ ejazaAttachmentIds: string[] = [];
+ fileList: IAttachment[] = [];
+ fileUploadModel: IFileUpload[] = [];
+ resMessage: BaseMessageModel = {};
+//===========end record===============
+  constructor(public dialog: MatDialog,public domSanitizer: DomSanitizer,private attachmentService: AttachmentsService,) { }
 
   ngOnInit(): void {
   }
@@ -173,6 +192,112 @@ onCountryChange(event:any){
 savePhonNumber(event:any){
   this.telInputParam.phoneNumber =event ;
 }
+
+// ===========record=========
+sanitize(url:string){
+  return this.domSanitizer.bypassSecurityTrustUrl(url);
+}
+/**
+* Start recording.
+*/
+initiateRecording() {
+
+  this.recording = true;
+  let mediaConstraints = {
+      video: false,
+      audio: true
+  };
+  navigator.mediaDevices
+      .getUserMedia(mediaConstraints)
+      .then(this.successCallback.bind(this), this.errorCallback.bind(this));
+}
+    /**
+     * Will be called automatically.
+     */
+     successCallback(stream:any) {
+      var options = {
+          mimeType: "audio/wav",
+          numberOfAudioChannels: 1
+      };
+      //Start Actuall Recording
+      var StereoAudioRecorder = RecordRTC.StereoAudioRecorder;
+      this.record = new StereoAudioRecorder(stream, options);
+      this.record.record();
+  }
+  /**
+   * Stop recording.
+   */
+  stopRecording() {
+      this.recording = false;
+      this.record.stop(this.processRecording.bind(this));
+  }
+    /**
+     * processRecording Do what ever you want with blob
+     * @param  {any} blob Blog
+     */
+     processRecording(blob:any) {
+      // this.url = URL.createObjectURL(blob);
+      // this.onEjazaFileChange(blob);
+
+
+      // const formData = new FormData();
+      //  formData.append('file', blob);
+      //  this. UploadFiles(formData);
+     //  this. UploadFiles(blob);
+
+     let files = Array<IFileUpload>();
+     files.push({
+       containerNameIndex : 1,
+       file: blob
+     })
+     this.UploadFiles(files)
+  }
+  /**
+   * Process Error.
+   */
+  errorCallback(error:any) {
+      this.error = 'Can not play audio in your browser';
+  }
+
+  // onEjazaFileChange(files: FileList) {
+  //   if (files.length > 0) {
+  //     Array.from(files).forEach(element => {
+  //       var fileUploadObj: IFileUpload = {
+  //         containerNameIndex: 1, // need to be changed based on file type
+  //         file: element
+
+  //       }
+  //       this.fileUploadModel.push(fileUploadObj)
+  //     });
+  //     this.UploadFiles(this.fileUploadModel);
+  //   }
+
+  // }
+
+  UploadFiles(files: any) {
+    if (files.length === 0) {
+      return;
+    }
+    this.attachmentService.upload(files).subscribe(
+      (res: any) => {
+        Array.from(res.data).forEach((elm: any) => {
+          this.ejazaAttachmentIds.push(elm.id);
+          this.fileList.push(elm);
+          this.url =elm.url ;
+        })
+        this.fileUploadModel = [];
+      }, error => {
+        console.log(error);
+        this.fileUploadModel = [];
+        this.resMessage = 
+        {
+          message: error,
+          type: BaseConstantModel.DANGER_TYPE
+        } 
+      }
+    )
+  }
+// =========end record===========
 
 }
 
