53a57e66ed9ecbe7481e5a776bc13d50465f0fd4
Mahmoud Elhaddad <melhaddad@arabdt.com>
2021-05-04 10:09:55 +0200

    delete question in exam form

--- src/app/modules/exam-form/components/exam-form-view/attache-exam-template/attache-exam-template.component.ts
+++ src/app/modules/exam-form/components/exam-form-view/attache-exam-template/attache-exam-template.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { AnswerTypeEnum } from 'src/app/core/enums/exam-builder-enums/answer-type-enum.enum';
@@ -12,6 +13,7 @@ import { BaseConstantModel } from 'src/app/core/ng-model/base-constant-model';
 import { BaseMessageModel } from 'src/app/core/ng-model/base-message-model';
 import { BaseResponseModel } from 'src/app/core/ng-model/base-response-model';
 import { ExamFormService } from 'src/app/core/services/exam-form-services/exam-form.service';
+import { ConfirmDialogModel, ConfirmModalComponent } from 'src/app/shared/components/confirm-modal/confirm-modal.component';
 
 @Component({
   selector: 'app-attache-exam-template',
@@ -19,130 +21,150 @@ import { ExamFormService } from 'src/app/core/services/exam-form-services/exam-f
   styleUrls: ['./attache-exam-template.component.scss']
 })
 export class AttacheExamTemplateComponent implements OnInit {
-  exam : IExam = {questions: []};
-  submitExam : boolean = false;
-  examJson:string | undefined;
-  voiceUrl:string | undefined;
-  isView=true;
+  exam: IExam = { questions: [] };
+  submitExam: boolean = false;
+  examJson: string | undefined;
+  voiceUrl: string | undefined;
+  isView = true;
   attacheExamTemplate: IAttacheExamTemplateModel = {};
-  @Input() selectedExamFormId={id:'',arabExamName:'',engExamName:''}; 
-  resultMessage:BaseMessageModel = {};
-  filterErrorMessage?:string;
-  errorMessage?:string;
-  successMessage?:string;
+  @Input() selectedExamFormId = { id: '', arabExamName: '', engExamName: '' };
+  resultMessage: BaseMessageModel = {};
+  filterErrorMessage?: string;
+  errorMessage?: string;
+  successMessage?: string;
   langEnum = LanguageEnum;
-  constructor( private examFormService: ExamFormService,private activeroute: ActivatedRoute, 
-    private router: Router, 
-    public translate: TranslateService,private fb: FormBuilder) { }
+  constructor(private examFormService: ExamFormService, 
+    private activeroute: ActivatedRoute,
+    public dialog: MatDialog,
+    private router: Router,
+    public translate: TranslateService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    if(this.selectedExamFormId.id!==undefined)
-    {
-     this.getAttacheExamTemplate(this.selectedExamFormId.id);
-     this.selectedExamFormId.id=this.selectedExamFormId.id;
+    if (this.selectedExamFormId.id !== undefined) {
+      this.getAttacheExamTemplate(this.selectedExamFormId.id);
+      this.selectedExamFormId.id = this.selectedExamFormId.id;
     }
   }
   ngOnChanges(changes: any) {
-    this.isView=true;
-    if(changes.selectedExamFormId?.currentValue.id!==undefined)
-   {
-    this.getAttacheExamTemplate(changes.selectedExamFormId.currentValue.id);
-    this.selectedExamFormId.id=changes.selectedExamFormId.currentValue.id;
-   }
-  
+    this.isView = true;
+    if (changes.selectedExamFormId?.currentValue.id !== undefined) {
+      this.getAttacheExamTemplate(changes.selectedExamFormId.currentValue.id);
+      this.selectedExamFormId.id = changes.selectedExamFormId.currentValue.id;
+    }
+
   }
   //questin:IQuestion |undefined;
-  addQuestion(){
-    if (Object.keys(this.exam).length === 0){
+  addQuestion() {
+    if (Object.keys(this.exam).length === 0) {
       let id = BaseConstantModel.newGuid();
-      this.exam = { id: id, questions : []}
+      this.exam = { id: id, questions: [] }
     }
 
     let qid = BaseConstantModel.newGuid();
-    let ques : IQuestion  = 
+    let ques: IQuestion =
     {
-      questionId : qid, 
-      questionNo : this.exam?.questions ? this.exam.questions.length + 1 : 1 ,
-      answers:[],
+      questionId: qid,
+      questionNo: this.exam?.questions ? this.exam.questions.length + 1 : 1,
+      answers: [],
       answerType: AnswerTypeEnum.singleSelect
     }
     this.exam.questions.push(ques);
   }
- 
-  saveExam(){
+
+  saveExam() {
     this.submitExam = true;
-    this.isView=true;
-   //  this.examJson = JSON.stringify(this.exam);
+    this.isView = true;
+    //  this.examJson = JSON.stringify(this.exam);
     // console.log(this.examJson);
     this.saveAttacheExamTemplate();
   }
 
-/////recording/////
-saveVoiceUrl(event:any){
-  this.voiceUrl = event;
-}
-/////end recording////
+  /////recording/////
+  saveVoiceUrl(event: any) {
+    this.voiceUrl = event;
+  }
+  /////end recording////
 
-editQuestionTemplate(){
-  this.isView=false;
-}
-getAttacheExamTemplate(examId?:string) {
-  this.filterErrorMessage = "";
-  this.resultMessage = {};
+  editQuestionTemplate() {
+    this.isView = false;
+  }
+  getAttacheExamTemplate(examId?: string) {
+    this.filterErrorMessage = "";
+    this.resultMessage = {};
 
-  if (examId){
-    this.examFormService.getExamFormDetails(examId).subscribe(res => {
-    
-      if (res.isSuccess) {
-        // response.data=<IExamFormsModel> response.data;
-       // this.exam=response.data.examTemplate;
-       this.exam = res.data as IExam;
-       this.exam.questions = res.data.examTemplate ? JSON.parse(res.data.examTemplate) : [];
-       // this.examJson = JSON.stringify(response.data.examTemplate);
+    if (examId) {
+      this.examFormService.getExamFormDetails(examId).subscribe(res => {
+
+        if (res.isSuccess) {
+          // response.data=<IExamFormsModel> response.data;
+          // this.exam=response.data.examTemplate;
+          this.exam = res.data as IExam;
+          this.exam.questions = res.data.examTemplate ? JSON.parse(res.data.examTemplate) : [];
+          // this.examJson = JSON.stringify(response.data.examTemplate);
+        }
+        else {
+          this.examJson = "";
+          this.filterErrorMessage = res.message;
+        }
+      },
+        error => {
+          this.resultMessage = {
+            message: error,
+            type: BaseConstantModel.DANGER_TYPE
+          }
+        }
+      )
+    }
+  }
+  saveAttacheExamTemplate() {
+    this.attacheExamTemplate.id = this.selectedExamFormId.id;
+    this.attacheExamTemplate.examTemplate = JSON.stringify(this.exam.questions);
+    this.errorMessage = '';
+    this.successMessage = '';
+    this.resultMessage = {};
+    this.examFormService.attachmentsExamTemplate(this.attacheExamTemplate).subscribe(res => {
+      let response = <BaseResponseModel>res;
+      if (response.isSuccess) {
+        this.resultMessage = {
+          message: res.message || "",
+          type: BaseConstantModel.SUCCESS_TYPE
+        }
+        this.getAttacheExamTemplate(this.attacheExamTemplate.id);
       }
       else {
-        this.examJson = "";
-        this.filterErrorMessage = res.message;
+        this.resultMessage = {
+          message: res.message,
+          type: BaseConstantModel.DANGER_TYPE
+        }
       }
     },
-    error => {
-      this.resultMessage ={
-        message: error,
-        type: BaseConstantModel.DANGER_TYPE
+      error => {
+        this.resultMessage = {
+          message: error,
+          type: BaseConstantModel.DANGER_TYPE
+        }
       }
-    }
     )
   }
-}
-saveAttacheExamTemplate(){
- this.attacheExamTemplate.id= this.selectedExamFormId.id;
- this.attacheExamTemplate.examTemplate=JSON.stringify(this.exam.questions);
-this.errorMessage = '';
-this.successMessage = '';
-this.resultMessage = {};
-this.examFormService.attachmentsExamTemplate(this.attacheExamTemplate).subscribe(res => {
-  let response = <BaseResponseModel>res;
-  if (response.isSuccess) {
-    this.resultMessage = {
-      message:res.message||"",
-      type: BaseConstantModel.SUCCESS_TYPE
-    }
-    this.getAttacheExamTemplate(this.attacheExamTemplate.id);
+
+  confirmDeleteQuestionDialog(question:IQuestion) {
+    const message = this.translate.currentLang === LanguageEnum.en ? "Are you sure that you want to delete question" : "هل متأكد من حذف هذا السؤال";
+
+    const dialogData = new ConfirmDialogModel(this.translate.currentLang === LanguageEnum.en ? 'Delete Question' : 'حذف السؤال', message);
+
+    const dialogRef = this.dialog.open(ConfirmModalComponent, {
+      maxWidth: "400px",
+      data: dialogData
+    });
+    dialogRef.afterClosed().subscribe(dialogResult => {
+      if (dialogResult == true) {
+        // let question = this.exam.questions.filter(q => q.questionNo == no)[0];
+        const index = this.exam.questions.indexOf(question);
+        if (index > -1){
+          this.exam.questions.splice(index,1);
+        }
+      }
+    });
   }
-  else {
-    this.resultMessage = {
-      message: res.message,
-      type: BaseConstantModel.DANGER_TYPE
-    }
-  }
-},
-error => {
-  this.resultMessage = {
-    message: error,
-    type: BaseConstantModel.DANGER_TYPE
-  }
-}
-)
-}
 
 }
