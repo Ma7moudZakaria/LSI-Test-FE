5ceb51e816e14a36537df4b339b8bc7d1fa22e53
Mahmoud Elhaddad <melhaddad@arabdt.com>
2021-05-08 10:07:53 +0200

    update admin scienitific problems

--- src/app/core/interfaces/scientific-problrm/iscientific-problems.ts
+++
@@ -1,8 +0,0 @@
-export interface IScientificProblems {
-    id?:string;
-    no?:number;
-    question?:string;
-    reply?:string;
-    question_number:number;
-    time:Date;
-}
--- src/app/modules/admin-messaging/components/admin-messaging-view/scientific-problems/scientific-problems.component.ts
+++ src/app/modules/admin-messaging/components/admin-messaging-view/scientific-problems/scientific-problems.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { ScientificProblemUsersEnum } from 'src/app/core/enums/scientific-problem-users-enum.enum';
+import { IScientificProblem } from 'src/app/core/interfaces/scientific-problrm/iscientific-problem';
 import { IScientificProblemFilter } from 'src/app/core/interfaces/scientific-problrm/iscientific-problem-filter';
-import { IScientificProblems } from 'src/app/core/interfaces/scientific-problrm/iscientific-problems';
+
 import { BaseConstantModel } from 'src/app/core/ng-model/base-constant-model';
 import { BaseMessageModel } from 'src/app/core/ng-model/base-message-model';
 import { BaseResponseModel } from 'src/app/core/ng-model/base-response-model';
@@ -13,20 +15,35 @@ import { ScientificProblemService } from 'src/app/core/services/scientific-probl
   styleUrls: ['./scientific-problems.component.scss']
 })
 export class ScientificProblemsComponent implements OnInit {
+
   scientificProblemFilter: IScientificProblemFilter = {};
   resultMessage:BaseMessageModel = {};
-  card_scientificProblem: IScientificProblems[]=[]; 
+  scientificProblems: IScientificProblem[] | undefined; 
+  numberItemsPerRow = 4;
+  adminCard : ScientificProblemUsersEnum = ScientificProblemUsersEnum.Admin;
+
   constructor(public translate: TranslateService,private scientificProblemService:ScientificProblemService) { }
+
   ngOnInit(): void {
+    this.getScientificProblems();
   }
-  getScientificProblemS(name?:string) {
+
+  getScientificProblems(name?:string) {
     this.scientificProblemFilter.filterText=name || '';
     this.scientificProblemFilter.skip=0;
     this.scientificProblemFilter.take= 2147483647;
     this.resultMessage = {};
+
     this.scientificProblemService.getScientificMateriaFilter(this.scientificProblemFilter).subscribe(res => {
-      let response = <BaseResponseModel>res;
-        this.card_scientificProblem = response.data;
+      if (res.isSuccess){
+        this.scientificProblems = res.data;
+      }
+      else{
+        this.resultMessage = {
+          message: res.message,
+          type: BaseConstantModel.DANGER_TYPE
+        }
+      }
     },
       error => {
         this.resultMessage = {
