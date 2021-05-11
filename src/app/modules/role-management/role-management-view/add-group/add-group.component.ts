f323aacc2915f82f08772ae7f037f7599a83783f
Mahmoud Elhaddad <melhaddad@arabdt.com>
2021-05-11 12:20:11 +0200

    fixing 1263

--- src/app/modules/role-management/role-management-view/add-group/add-group.component.ts
+++ src/app/modules/role-management/role-management-view/add-group/add-group.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BaseConstantModel } from 'src/app/core/ng-model/base-constant-model';
+import { BaseMessageModel } from 'src/app/core/ng-model/base-message-model';
 import { AlertifyService } from 'src/app/core/services/alertify-services/alertify.service';
 import { RoleManagementService } from 'src/app/core/services/role-management/role-management.service';
 
@@ -12,6 +14,7 @@ export class AddGroupComponent implements OnInit {
   DataForm!: FormGroup;
   isSubmit: boolean = false;
   @Output() hideform = new EventEmitter<boolean>();
+  resultMessage: BaseMessageModel = {};
 
   constructor(
     private formBuilder: FormBuilder,
@@ -39,8 +42,22 @@ export class AddGroupComponent implements OnInit {
       return;
     }
     this.RoleManagement.createRole(this.DataForm.value).subscribe((res) => {
+      if (res.isSuccess){
+        
       this._alertify.success(res.message || "");
       this.backList();
+      }
+      else{
+        this.resultMessage = {
+          message: res.message,
+          type: BaseConstantModel.DANGER_TYPE
+        }
+      }
+    },error => {
+      this.resultMessage = {
+        message: error,
+        type: BaseConstantModel.DANGER_TYPE
+      }
     });
   }
 
