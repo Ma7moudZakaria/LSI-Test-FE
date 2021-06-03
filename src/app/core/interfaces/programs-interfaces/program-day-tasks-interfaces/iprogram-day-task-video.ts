9023a78149ddfff978a39a5ef91d329b17593f2d
alaa <ahaggag@arabdt.com>
2021-06-03 17:53:26 +0200

    done to structure to program day task

---
+++ src/app/core/interfaces/programs-interfaces/program-day-tasks-interfaces/iprogram-day-task-video.ts
@@ -0,0 +1,7 @@
+import { IAttachment } from "../../attachments-interfaces/iattachment";
+
+export interface IProgramDayTaskVideo {
+    videoName?:string;
+    vidio?:IAttachment;
+    videoLink?:string;
+}
