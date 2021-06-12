c0562d67108e241d3ceb497aa957557db5323f8a
alaa <ahaggag@arabdt.com>
2021-06-12 17:03:16 +0200

    donde sard

--- src/app/shared/components/program-day-tasks/program-day-task-recitation/program-day-task-recitation.component.ts
+++ src/app/shared/components/program-day-tasks/program-day-task-recitation/program-day-task-recitation.component.ts
@@ -1,5 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { ProgramDayTaskRecitationType } from 'src/app/core/enums/program-day-task-recitation-type.enum';
+import { ILookupCollection } from 'src/app/core/interfaces/lookup/ilookup-collection';
+import { IRecitationTimes } from 'src/app/core/interfaces/programs-interfaces/iprogram-basic-info-model';
 import { IProgramBasicInfoDetails } from 'src/app/core/interfaces/programs-interfaces/iprogram-details';
+import { BaseConstantModel } from 'src/app/core/ng-model/base-constant-model';
+import { BaseMessageModel } from 'src/app/core/ng-model/base-message-model';
+import { LookupService } from 'src/app/core/services/lookup-services/lookup.service';
 
 @Component({
   selector: 'app-program-day-task-recitation',
@@ -7,10 +14,34 @@ import { IProgramBasicInfoDetails } from 'src/app/core/interfaces/programs-inter
   styleUrls: ['./program-day-task-recitation.component.scss']
 })
 export class ProgramDayTaskRecitationComponent implements OnInit {
-  @Input() recitationDetailsModel: IProgramBasicInfoDetails = {}
-  constructor() { }
+  @Input() recitationDetailsModel: IProgramBasicInfoDetails = {};
+  collectionOfLookup = {} as ILookupCollection;
+  listOfLookup: string[] = ['SARD_TYPES'];
+  resultMessage: BaseMessageModel = {};
+  progRecitationTimes:IRecitationTimes[] = [];
+  constructor( 
+     public translate: TranslateService,
+    private lookupService: LookupService) { }
 
   ngOnInit(): void {
+    this.getLookupByKey();
+    this.progRecitationTimes = this.recitationDetailsModel?.prgRecitTms ? 
+    this.recitationDetailsModel?.prgRecitTms.filter(i=>i.huffno !== ProgramDayTaskRecitationType.limited).map((item: any) => ({ progRecFrom:item.recitFrom, progRecTo:item.recitTo })) : [];
+
+  }
+  getLookupByKey() {
+    this.lookupService.getLookupByKey(this.listOfLookup).subscribe(res => {
+      if (res.isSuccess) {
+        this.collectionOfLookup = res.data as ILookupCollection;
+      }
+      else {
+        this.resultMessage =
+        {
+          message: res.message,
+          type: BaseConstantModel.DANGER_TYPE
+        }
+      }
+    });
   }
 
 }
