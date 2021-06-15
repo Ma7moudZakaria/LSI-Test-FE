45d2bf1ddf741772bb79a143625afc05c8a74066
Mahmoud Elhaddad <melhaddad@arabdt.com>
2021-06-15 19:10:59 +0200

    fixing 1886

--- src/app/modules/program/components/add-program/program-notifacations/program-notifacations.component.ts
+++ src/app/modules/program/components/add-program/program-notifacations/program-notifacations.component.ts
@@ -27,7 +27,7 @@ export class ProgramNotifacationsComponent implements OnInit {
 
   closeNotifyProgram() {
     this.showAddEditOverlay = false;
-    this.ProgramNotificationChild?.getAllNotifications(this.progId);
+    this.ProgramNotificationChild?.getAllNotifications();
   }
 
 
