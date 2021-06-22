2f05db4a88c57038d19734c53dc241195ff8ec2e
alaa <ahaggag@arabdt.com>
2021-06-22 10:25:50 +0200

    prog cond in design

--- src/app/core/enums/programs/program-predefined-conditions-enum.enum.ts
+++ src/app/core/enums/programs/program-predefined-conditions-enum.enum.ts
@@ -1,9 +1,9 @@
 export enum programPredefinedConditionsEnum {
-    dgreeaLastProgram = 1,
+    memorize = 1,
     age = 2,
     qualifications = 3,
     programFinished = 4,
     numberStudentSubscribtion = 5,
     memorizeQuran = 6,
-
+    dgreeaLastProgram=7
 }
