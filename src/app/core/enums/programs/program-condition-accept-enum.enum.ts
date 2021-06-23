bba613aebdcfa2a99baaf83920528d1f5ef1929a
alaa <ahaggag@arabdt.com>
2021-06-23 17:04:41 +0200

    add

---
+++ src/app/core/enums/programs/program-condition-accept-enum.enum.ts
@@ -0,0 +1,7 @@
+export enum ProgramConditionAcceptEnum {
+    memorize="memorize only",
+    readOnly="Read  only",
+    memorizeAndRead=" memorize and read",
+    moreProgramMemorize="More of Program Memorize",
+    moreProgramRead=" More of Program Read"
+}
--- src/app/shared/components/setting-conditions/setting-accept/setting-accept.component.ts
+++ src/app/shared/components/setting-conditions/setting-accept/setting-accept.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { LanguageEnum } from 'src/app/core/enums/language-enum.enum';
+import { ProgramConditionAcceptEnum } from 'src/app/core/enums/programs/program-condition-accept-enum.enum';
 import { IprogramPredefinedCustomConditionsModel } from 'src/app/core/interfaces/programs-interfaces/iprogram-predefined-custom-conditions-model';
 
 @Component({
@@ -8,7 +11,11 @@ import { IprogramPredefinedCustomConditionsModel } from 'src/app/core/interfaces
 })
 export class SettingAcceptComponent implements OnInit {
   @Input() item: IprogramPredefinedCustomConditionsModel = {}
-  constructor() { }
+  programConditionAcceptEnum = ProgramConditionAcceptEnum;
+  langEnum = LanguageEnum;
+  constructor(
+    public translate: TranslateService
+  ) { }
 
   ngOnInit(): void {
   }
