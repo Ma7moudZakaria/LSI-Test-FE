ae44183166103c5a21d3459221b15527059291fd
nermenOsama123 <70470826+nermenOsama123@users.noreply.github.com>
2021-07-12 14:30:19 +0200

    teacher-request

---
+++ src/app/modules/admin-messaging/components/student-program-request-view/student-program-request-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-program-request-view',
+  templateUrl: './student-program-request-view.component.html',
+  styleUrls: ['./student-program-request-view.component.scss']
+})
+export class StudentProgramRequestViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/admin-messaging/components/teacher-program-request-view/teacher-program-request-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-program-request-view',
+  templateUrl: './teacher-program-request-view.component.html',
+  styleUrls: ['./teacher-program-request-view.component.scss']
+})
+export class TeacherProgramRequestViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
