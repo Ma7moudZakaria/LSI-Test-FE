c785f231550f5be4ece8f79627f489a9ed5158ad
Mohamed Nagy <mnagy@arabdt.com>
2021-07-25 10:46:12 +0200

    update profile

--- src/app/shared/components/milady-hijri-calendar/milady-hijri-calendar.component.ts
+++ src/app/shared/components/milady-hijri-calendar/milady-hijri-calendar.component.ts
@@ -1,24 +1,25 @@
-import { Observable } from 'rxjs';
-import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
-import { DateFormatterService, DateType } from 'ngx-hijri-gregorian-datepicker';
-import { NgbModal, NgbDateStruct, NgbDate } from '@ng-bootstrap/ng-bootstrap';
-import { TranslateService } from '@ngx-translate/core';
-import { LanguageEnum } from 'src/app/core/enums/language-enum.enum';
-import { LanguageService } from 'src/app/core/services/language-services/language.service';
+import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {DateFormatterService, DateType} from 'ngx-hijri-gregorian-datepicker';
+import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+import {TranslateService} from '@ngx-translate/core';
+import {LanguageEnum} from 'src/app/core/enums/language-enum.enum';
+import {LanguageService} from 'src/app/core/services/language-services/language.service';
+import {BaseSelectedDateModel} from "../../../core/ng-model/base-selected-date-model";
+
 @Component({
   selector: 'app-milady-hijri-calendar',
   templateUrl: './milady-hijri-calendar.component.html',
   styleUrls: ['./milady-hijri-calendar.component.scss'],
 })
 export class MiladyHijriCalendarComponent implements OnInit {
-  
+
   @ViewChild('hijGeo') hijGeoChildComp: any | undefined; //any instead ElementReg to can access property
   // dateFrom!: NgbDateStruct;
   @Input() dateTo!: NgbDateStruct;
 
   selectedDateType_Melady = DateType.Gregorian;  // or DateType.Gregorian
   selectedDateType_Hijri = DateType.Hijri;
-
+  calenderType:any;
   dateFromString: string = '';
   maxGreg!: NgbDateStruct;
   maxHijri!: NgbDateStruct;
@@ -33,14 +34,23 @@ export class MiladyHijriCalendarComponent implements OnInit {
 
   @Output() sendDate = new EventEmitter;
 
+  dataSend : BaseSelectedDateModel = new BaseSelectedDateModel();
   //  @Input() item: { title: string, state: boolean };
+  @Input() editcalenderType: any;
   constructor(public translate: TranslateService , public languageService: LanguageService,
-    private dateFormatterService: DateFormatterService) { }
+    private dateFormatterService: DateFormatterService) {
+
+  }
 
   ngOnInit(): void {
     // this.minHijri = Date.now() || 2020;
     this.setCurrentLang();
     this.setHijri();
+    if (this.editcalenderType){
+      this.calenderType = this.editcalenderType.selectedDateType;
+      console.log("this.calenderType",this.editcalenderType);
+    }
+
   }
 
   setHijri() {
@@ -67,8 +77,7 @@ export class MiladyHijriCalendarComponent implements OnInit {
   }
 
   emitData(data: any) {
-    console.log(data)
-    console.log(this.hijGeoChildComp.selectedDateType);
+    // console.log(this.hijGeoChildComp.selectedDateType);
     // let DateNow = Date.now();
 
     // // let YearDate = DateNow.toString("yyyy mm dd");
@@ -76,10 +85,11 @@ export class MiladyHijriCalendarComponent implements OnInit {
     // if(data > DateNow){
     //   this.sendDate.emit(data)
     // }
-
-
-    this.sendDate.emit(data)
+    this.dataSend.selectedDateValue = data;
+    this.dataSend.selectedDateType = this.hijGeoChildComp.selectedDateType;
+   // this.dataSend.selectedDateType == 1? this.dataSend.calendarType = this.selectedDateType_Hijri : this.dataSend.calendarType = this.selectedDateType_Melady;
+   //  console.log("this.dataSend", this.dataSend)
+    this.sendDate.emit(this.dataSend)
   }
-
 }
 
