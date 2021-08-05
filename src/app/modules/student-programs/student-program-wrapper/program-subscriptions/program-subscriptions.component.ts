1461df37e566006a3785a64aefd8638bf25d6573
nermenOsama123 <70470826+nermenOsama123@users.noreply.github.com>
2021-08-05 18:15:58 +0200

    student-programs

--- src/app/modules/student-programs/student-program-wrapper/program-subscriptions/program-subscriptions.component.ts
+++ src/app/modules/student-programs/student-program-wrapper/program-subscriptions/program-subscriptions.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { IProgramsForStudentSubscriptionsModel } from 'src/app/core/interfaces/student-program-subscription-interfaces/iprograms-for-student-subscriptions-model';
+import { IProgramsForStudentsSubscriptionsFilterRequestModel } from 'src/app/core/interfaces/student-program-subscription-interfaces/iprograms-for-students-subscriptions-filter-request-model';
+import { StudentProgramSubscriptionServicesService } from 'src/app/core/services/student-program-subscription-services/student-program-subscription-services.service';
+import { BaseResponseModel } from 'src/app/core/ng-model/base-response-model';
+import { LanguageEnum } from 'src/app/core/enums/language-enum.enum';
+import { AlertifyService } from 'src/app/core/services/alertify-services/alertify.service';
 
 @Component({
   selector: 'app-program-subscriptions',
@@ -6,10 +13,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./program-subscriptions.component.scss']
 })
 export class ProgramSubscriptionsComponent implements OnInit {
+  programsForStudentSubscriptionsFilterRequestModel: IProgramsForStudentsSubscriptionsFilterRequestModel | undefined;
+  programsForStudentSubscriptionsList: IProgramsForStudentSubscriptionsModel[] | undefined;
+  errorMessage?: string;
+  langEnum = LanguageEnum;
 
-  constructor() { }
+  constructor(
+    public translate: TranslateService, private alertify: AlertifyService,
+    private studentProgramSubscriptionServicesService: StudentProgramSubscriptionServicesService,
+  ) { }
 
   ngOnInit(): void {
+    this.getProgramsForStudentssSubscriptions()
   }
-  list_programs = [1, 2, 3]
+
+  getProgramsForStudentssSubscriptions() {
+    this.studentProgramSubscriptionServicesService.getProgramsForStudentsSubscriptions(this.programsForStudentSubscriptionsFilterRequestModel || {}).subscribe(res => {
+      // var response = <BaseResponseModel>res;
+      if (res.isSuccess) {
+        this.programsForStudentSubscriptionsList = res.data;
+        // this.programsForStudentSubscriptionsList = res.data as IProgramsForStudentSubscriptionsModel[];
+
+
+      }
+      else {
+        this.alertify.error(res.message || '');
+
+      }
+    },
+      error => {
+        console.log(error);
+      });
+  }
+
 }
