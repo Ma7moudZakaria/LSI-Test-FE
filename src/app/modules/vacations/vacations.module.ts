67072eea994530b3baeacf8588b9d4ee734545d4
Mohamed Nagy <mnagy@arabdt.com>
2021-08-10 18:15:29 +0200

    student program vacation requests

---
+++ src/app/modules/vacations/vacations.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VacationsRoutingModule } from './vacations-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    VacationsRoutingModule
+  ]
+})
+export class VacationsModule { }
