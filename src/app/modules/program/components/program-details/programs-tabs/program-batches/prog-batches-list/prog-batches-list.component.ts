990b82b6c546252040e9f48a53f72a8b7bb6fe5c
Mahmoud Elhaddad <melhaddad@arabdt.com>
2021-08-11 12:18:41 +0200

    add program batch

--- src/app/modules/program/components/program-details/programs-tabs/program-batches/prog-batches-list/prog-batches-list.component.ts
+++ src/app/modules/program/components/program-details/programs-tabs/program-batches/prog-batches-list/prog-batches-list.component.ts
@@ -1,5 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { error } from 'protractor';
 import { IProgramDetails } from 'src/app/core/interfaces/programs-interfaces/iprogram-details';
+import { AlertifyService } from 'src/app/core/services/alertify-services/alertify.service';
+import {ProgramBatchesService} from 'src/app/core/services/program-batches-service/program-batches.service'
 
 @Component({
   selector: 'app-prog-batches-list',
@@ -12,13 +16,29 @@ export class ProgBatchesListComponent implements OnInit {
 
   @Input() programDetails : IProgramDetails | undefined ;
 
-  constructor() { }
+  constructor(public translate: TranslateService,
+    private programBatchesService: ProgramBatchesService,
+    private alertifyService : AlertifyService) { }
 
   ngOnInit(): void {
   }
 
   showAddBatchOverlay(){
     this.showAddBatchOverlayEvent.emit(true);
+  }
+
+  updateProgBatchesListAfterAdd(){
+    this.programDetails && this.programDetails.progBaseInfo && this.programDetails.progBaseInfo.id ?
+    this.programBatchesService.getProgBatchesByProgId(this.programDetails?.progBaseInfo?.id).subscribe(res=>{
+      if (res.isSuccess && this.programDetails){
+        this.programDetails.progBats = res.data
+      }
+      else{
+        this.alertifyService.error(res.message || '');
+      }
+    },error => {
+
+    }):'';
   }
 
 }
