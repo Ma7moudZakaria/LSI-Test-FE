e1631c44f3b630b1659f6f7b7400c30fd4f69a36
nermenOsama123 <70470826+nermenOsama123@users.noreply.github.com>
2021-08-11 16:33:02 +0200

    change in student

--- src/app/modules/student-program-subscription/student-program-sub-view/student-program-sub-details/student-program-sub-details.component.ts
+++ src/app/modules/student-program-subscription/student-program-sub-view/student-program-sub-details/student-program-sub-details.component.ts
@@ -1,27 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import {BaseMessageModel} from '../../../../core/ng-model/base-message-model';
-import {IProgramsForTeacherSubscriptionsModel} from '../../../../core/interfaces/teacher-program-subscription-interfaces/iprograms-for-teacher-subscriptions-model';
-import {TeacherProgramSubscriptionServicesService} from '../../../../core/services/teacher-program-subscription-services/teacher-program-subscription-services.service';
-import {BaseResponseModel} from '../../../../core/ng-model/base-response-model';
-import {BaseConstantModel} from '../../../../core/ng-model/base-constant-model';
+import { BaseMessageModel } from '../../../../core/ng-model/base-message-model';
+import { IProgramsForTeacherSubscriptionsModel } from '../../../../core/interfaces/teacher-program-subscription-interfaces/iprograms-for-teacher-subscriptions-model';
+import { TeacherProgramSubscriptionServicesService } from '../../../../core/services/teacher-program-subscription-services/teacher-program-subscription-services.service';
+import { BaseResponseModel } from '../../../../core/ng-model/base-response-model';
+import { BaseConstantModel } from '../../../../core/ng-model/base-constant-model';
 
-import {ActivatedRoute} from '@angular/router';
-import {IProgramSubscriptionDetails} from '../../../../core/interfaces/teacher-program-subscription-interfaces/iprogram-subscription-details';
+import { ActivatedRoute } from '@angular/router';
+import { IProgramSubscriptionDetails } from '../../../../core/interfaces/teacher-program-subscription-interfaces/iprogram-subscription-details';
+import { ITeacherSubmitSubscriptinoModel } from 'src/app/core/interfaces/teacher-program-subscription-interfaces/iteacher-submit-subscriptino-model';
+import { Iuser } from 'src/app/core/interfaces/user-interfaces/iuser';
 @Component({
   selector: 'app-student-program-sub-details',
   templateUrl: './student-program-sub-details.component.html',
   styleUrls: ['./student-program-sub-details.component.scss']
 })
 export class StudentProgramSubDetailsComponent implements OnInit {
-  checkbox = false;
-  changeCheck(e: any){
-    if(e.checked==true){
-      this.checkbox = true;
-    } else {
-      this.checkbox = false;
-    }
-
-  };
+  submitSubscritionModel = {} as ITeacherSubmitSubscriptinoModel;
+  disabledCheck: boolean = true
+  currentUser: Iuser | undefined;
   ProgramSubscriptionId: string = "";
   errorMessage?: string;
   resMessage: BaseMessageModel = {};
@@ -30,12 +26,12 @@ export class StudentProgramSubDetailsComponent implements OnInit {
 
   constructor(
     private TeacherProgramSubscriptionServicesService: TeacherProgramSubscriptionServicesService,
-    private route:ActivatedRoute
+    private route: ActivatedRoute
 
   ) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe( params => this.ProgramSubscriptionId =params['id']);
+    this.route.params.subscribe(params => this.ProgramSubscriptionId = params['id']);
     this.getSubscriptionProgramDetails();
   }
   getSubscriptionProgramDetails() {
@@ -45,7 +41,7 @@ export class StudentProgramSubDetailsComponent implements OnInit {
         var response = <BaseResponseModel>res;
         if (response.isSuccess) {
           this.programsForSubscriptionsDetails = response.data;
-          console.log('this.programsForSubscriptionsDetails',this.programsForSubscriptionsDetails);
+          console.log('this.programsForSubscriptionsDetails', this.programsForSubscriptionsDetails);
         }
         else {
           this.resMessage = {
@@ -60,5 +56,19 @@ export class StudentProgramSubDetailsComponent implements OnInit {
         }
       })
   }
+
+  checkConditions(event: any) {
+
+    if (event.checked) {
+      this.disabledCheck = false
+    }
+    else {
+      this.disabledCheck = true
+
+    }
+
+
+  }
+
 }
 
