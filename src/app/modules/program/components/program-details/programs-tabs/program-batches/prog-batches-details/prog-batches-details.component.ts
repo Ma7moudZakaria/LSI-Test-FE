f75d987b74a54badfa53989ad00e86f696b8931c
Mahmoud-m14 <mmahmoud@arabdt.com>
2021-08-17 16:51:00 +0200

    edit batches design

--- src/app/modules/program/components/program-details/programs-tabs/program-batches/prog-batches-details/prog-batches-details.component.ts
+++ src/app/modules/program/components/program-details/programs-tabs/program-batches/prog-batches-details/prog-batches-details.component.ts
@@ -9,7 +9,7 @@ import { IProgramDetails } from 'src/app/core/interfaces/programs-interfaces/ipr
 export class ProgBatchesDetailsComponent implements OnInit {
 
   @Input() programDetails : IProgramDetails | undefined ;
-  
+  test = [0,1,2,3,4,5,6,7,8,9];
   showTap: string = 'teacher';
 
   constructor() { }
