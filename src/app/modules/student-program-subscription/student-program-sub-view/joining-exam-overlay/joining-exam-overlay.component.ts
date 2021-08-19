3edd67ad23df14aa5a6ad0fed9ada473be009c00
nermenOsama123 <70470826+nermenOsama123@users.noreply.github.com>
2021-08-19 19:16:59 +0200

    join exam in stu sub

--- src/app/modules/student-program-subscription/student-program-sub-view/joining-exam-overlay/joining-exam-overlay.component.ts
+++ src/app/modules/student-program-subscription/student-program-sub-view/joining-exam-overlay/joining-exam-overlay.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { AnswerTypeEnum } from 'src/app/core/enums/exam-builder-enums/answer-type-enum.enum';
 import { LanguageEnum } from 'src/app/core/enums/language-enum.enum';
+import { IQuestion } from 'src/app/core/interfaces/exam-builder-interfaces/iquestion';
 import { IRandomExamModel } from 'src/app/core/interfaces/student-program-subscription-interfaces/irandom-exam-model';
 import { IProgramSubscriptionDetails } from 'src/app/core/interfaces/teacher-program-subscription-interfaces/iprogram-subscription-details';
 import { BaseConstantModel } from 'src/app/core/ng-model/base-constant-model';
@@ -17,9 +19,13 @@ export class JoiningExamOverlayComponent implements OnInit {
 
   @Input() progDetails: IProgramSubscriptionDetails | undefined;
   resMessage: BaseMessageModel = {};
-  randomExam:IRandomExamModel|undefined;
+  randomExam: IRandomExamModel | undefined;
   langEnum = LanguageEnum;
-  
+  answerTypeEnum = AnswerTypeEnum;
+
+  currentQuestion: IQuestion | undefined
+  counter: number = 0
+
   constructor(
     private studentCustomConditionService: StudentProgramSubscriptionServicesService,
     public translate: TranslateService,
@@ -27,18 +33,27 @@ export class JoiningExamOverlayComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    // if (this.currentQuestion
+    //   && this.currentQuestion.studentAnswersByAnswerNumbers
+    //   && this.currentQuestion.studentAnswersByAnswerNumber) {
+    //   this.currentQuestion.studentAnswersByAnswerNumber = '';
+    //   this.currentQuestion.studentAnswersByAnswerNumbers = [];
+    // }
 
     this.getRandomExam();
   }
+
 
   getRandomExam() {
     this.studentCustomConditionService.getRandomJoiningExam(this.progDetails?.id || '').subscribe(
       res => {
 
         if (res.isSuccess) {
-           this.randomExam = res.data as IRandomExamModel;
-           this.randomExam.questions = res.data.templateExam ? JSON.parse( res.data.templateExam) : [];
-       
+          this.randomExam = res.data as IRandomExamModel;
+          this.randomExam.questions = res.data.templateExam ? JSON.parse(res.data.templateExam) : [];
+
+          this.currentQuestion = this.randomExam && this.randomExam.questions ?
+            this.randomExam.questions[this.counter] : undefined;
         }
         else {
           this.resMessage = {
@@ -52,6 +67,11 @@ export class JoiningExamOverlayComponent implements OnInit {
           type: BaseConstantModel.DANGER_TYPE
         }
       })
+  }
+  NextQuestion() {
+    this.counter++;
+    this.currentQuestion = this.randomExam && this.randomExam.questions ?
+      this.randomExam.questions[this.counter] : undefined;
   }
 
 
