ca4d87f0aec8fb1e009d8eba21429014ff8fb661
nermenOsama123 <70470826+nermenOsama123@users.noreply.github.com>
2021-09-02 17:51:10 +0200

    drop out request teacher and student

--- src/app/shared/components/admin-vacation-request-for-student-tab-card/admin-vacation-request-for-student-tab-card.component.ts
+++ src/app/shared/components/admin-vacation-request-for-student-tab-card/admin-vacation-request-for-student-tab-card.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { LanguageEnum } from 'src/app/core/enums/language-enum.enum';
+import { StudentProgramVacationStatusEnum } from 'src/app/core/enums/StudentProgramVacationStatus/student-program-vacation-status.enum';
+import { IStudentProgramVacationStudentViewModel } from 'src/app/core/interfaces/student-program-vacation-interfaces/istudent-program-vacation-student-view-model';
 
 @Component({
   selector: 'app-admin-vacation-request-for-student-tab-card',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminVacationRequestForStudentTabCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() studentProgramVacationStudentViewModel: IStudentProgramVacationStudentViewModel = { totalRows: 0 }
+  // @Output() CancelStudentVacationRequest = new EventEmitter<IStudentProgramVacationStudentViewModel>();
+  @Output() TerminateStudentVacationRequest = new EventEmitter<IStudentProgramVacationStudentViewModel>();
+
+  studentProgramVacationStatus = StudentProgramVacationStatusEnum;
+  requestStatus?: string;
+  langEnum = LanguageEnum;
+
+  constructor(public translate: TranslateService) { }
 
   ngOnInit(): void {
   }
+
+  // cancelStudentReq() {
+  //   this.CancelStudentVacationRequest.emit(this.studentProgramVacationStudentViewModel);
+  // }
+  // terminateStudentReq() {
+  //   this.TerminateStudentVacationRequest.emit(this.studentProgramVacationStudentViewModel);
+  // }
 
 }
--- src/app/shared/components/admin-vacation-request-for-student-tab-grid/admin-vacation-request-for-student-tab-grid.component.ts
+++ src/app/shared/components/admin-vacation-request-for-student-tab-grid/admin-vacation-request-for-student-tab-grid.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IStudentProgramVacationModel } from '../../../core/interfaces/student-program-vacation-interfaces/i-student-program-vacation-model';
+import { StudentProgramVacationStatusEnum } from '../../../core/enums/StudentProgramVacationStatus/student-program-vacation-status.enum';
+import { IStudentProgramVacationFilterRequestModel } from '../../../core/interfaces/student-program-vacation-interfaces/i-student-program-vacation-filter-request-model';
+import { LanguageEnum } from '../../../core/enums/language-enum.enum';
+import { TranslateService } from '@ngx-translate/core';
+import { ExportationService } from '../../../core/services/exportation-services/exportation.service';
+import { IUser } from '../../../core/interfaces/auth-interfaces/iuser-model';
+import { IStudentProgramVacationStudentViewModel } from '../../../core/interfaces/student-program-vacation-interfaces/istudent-program-vacation-student-view-model';
+import { ITeacherStudentViewModel } from '../../../core/interfaces/teacher-drop-out-request-interfaces/Iteacher-student-model';
 
 @Component({
   selector: 'app-admin-vacation-request-for-student-tab-grid',
@@ -7,9 +16,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminVacationRequestForStudentTabGridComponent implements OnInit {
 
-  constructor() { }
+  @Input() vacationTypeEnum: StudentProgramVacationStatusEnum = StudentProgramVacationStatusEnum.Pending;
+  @Input() studentVacationItems: IStudentProgramVacationModel[] = []
+  @Input() studentVacationFilterRequestModel: IStudentProgramVacationFilterRequestModel = { skip: 0, take: 9, page: 1 };
+  @Output() studentVacationFilterEvent = new EventEmitter<IStudentProgramVacationFilterRequestModel>();
+  @Output() acceptAllStudentProgramVacationChecked = new EventEmitter<IStudentProgramVacationModel>();
+  @Output() acceptStudentProgramVacation = new EventEmitter<IStudentProgramVacationModel>();
+  @Output() sendStudentVacationId = new EventEmitter<ITeacherStudentViewModel>();
+  @Input() numberPerRow: number = 3;
+  @Input() totalCount: number = 0;
+  @Input() studentProgramVacationFilterRequestModel: IStudentProgramVacationFilterRequestModel = { skip: 0, take: 9, page: 1 };
+  @Output() studentProgramVacationStudentViewModel = new EventEmitter<IStudentProgramVacationStudentViewModel>();
+  @Output() terminateStudentProgramVacation = new EventEmitter<IStudentProgramVacationStudentViewModel>();
+  @Output() cancelStudentProgramVacation = new EventEmitter<IStudentProgramVacationStudentViewModel>();
+
+  orderTypeToggel = 1;
+  allSelected: boolean = false;
+  stuTabTypeSelected = StudentProgramVacationStatusEnum;
+  currentUser: IUser | undefined;
+  userRole: any;
+
+  constructor(public translate: TranslateService,
+  ) { }
 
   ngOnInit(): void {
+    this.currentUser = JSON.parse(localStorage.getItem("user") as string) as IUser;
+    this.userRole = this.currentUser.usrRoles?.usrRoles?.[0].enRoleName.toString();
+
   }
+  // getStudentVacationId(event: ITeacherStudentViewModel) {
+  //   this.sendStudentVacationId.emit(event);
+  // }
+
+
+
+
+  enableStudentSelectOperations(): boolean {
+    return this.studentVacationItems.filter(t => t.checked).length > 0 || this.allSelected;
+  }
+
+
+
+
+  onStudentPageChange() {
+    this.studentProgramVacationFilterRequestModel.skip = (this.studentProgramVacationFilterRequestModel.page - 1) * (this.studentProgramVacationFilterRequestModel.take);
+    this.studentVacationFilterEvent.emit(this.studentProgramVacationFilterRequestModel);
+    // this.setStudentAllChecked(false);
+  }
+
+
+
+  // terminateStuRequest(event: IStudentProgramVacationStudentViewModel) {
+  //   this.terminateStudentProgramVacation.emit(event)
+
+  // }
+
+
+  // cancelStuRequest(event: IStudentProgramVacationStudentViewModel) {
+  //   this.cancelStudentProgramVacation.emit(event)
+
+  // }
 
 }
