30fda74644aff846402e20d4dfd27bf829caf189
Mohamed Nagy <mnagy@arabdt.com>
2021-09-20 16:17:03 +0200

    advanced search

---
+++ src/app/modules/admin-messaging/components/scientific-problems-view/scientific-problem-advanced-search/scientific-problem-advanced-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scientific-problem-advanced-search',
+  templateUrl: './scientific-problem-advanced-search.component.html',
+  styleUrls: ['./scientific-problem-advanced-search.component.scss']
+})
+export class ScientificProblemAdvancedSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
