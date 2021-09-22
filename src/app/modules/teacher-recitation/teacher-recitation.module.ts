8ac1f9a9236c517f240a8cca39bf41cdb79af2ab
nermenOsama123 <70470826+nermenOsama123@users.noreply.github.com>
2021-09-22 19:54:01 +0200

    remove teacher recitation join request

--- src/app/modules/teacher-recitation/teacher-recitation.module.ts
+++ src/app/modules/teacher-recitation/teacher-recitation.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { TeacherRecitationRoutingModule } from './teacher-recitation-routing.module';
 import { TeacherRecitationGroupsComponent } from './components/teacher-recitation-wrapper/teacher-recitation-groups/teacher-recitation-groups.component';
 import { TeacherRecitationGroupSelectedComponent } from './components/teacher-recitation-wrapper/teacher-recitation-group-selected/teacher-recitation-group-selected.component';
-import { TeacherRecitationJoinRequestComponent } from './components/teacher-recitation-wrapper/teacher-recitation-join-request/teacher-recitation-join-request.component';
 import { AddNewGroupTeacherRecitationComponent } from './components/teacher-recitation-wrapper/add-new-group-teacher-recitation/add-new-group-teacher-recitation.component';
 import { TeacherRecitationWrapperComponent } from './components/teacher-recitation-wrapper/teacher-recitation-wrapper.component';
 import { SharedModule } from 'src/app/shared/shared.module';
@@ -14,7 +13,8 @@ import { MatListModule } from '@angular/material/list';
 import { RejectedGroupRequestFormComponent } from './components/teacher-recitation-wrapper/rejected-group-request-form/rejected-group-request-form.component';
 
 @NgModule({
-  declarations: [TeacherRecitationGroupsComponent, TeacherRecitationGroupSelectedComponent, TeacherRecitationJoinRequestComponent, AddNewGroupTeacherRecitationComponent, TeacherRecitationWrapperComponent, RejectedGroupRequestFormComponent],
+  declarations: [TeacherRecitationGroupsComponent, TeacherRecitationGroupSelectedComponent,
+    AddNewGroupTeacherRecitationComponent, TeacherRecitationWrapperComponent, RejectedGroupRequestFormComponent],
   imports: [
     CommonModule, SharedModule,
     TeacherRecitationRoutingModule, MatListModule,
